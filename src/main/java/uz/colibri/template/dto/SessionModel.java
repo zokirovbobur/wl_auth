a3c5a075cea337ff04d3eec9ffb7f653aa448619
zokirovbobur93 <zokirovbobur93@gmail.com>
2019-12-01 16:56:27 +0500

    sessionId generate

--- src/main/java/uz/colibri/template/dto/SessionModel.java
+++ src/main/java/uz/colibri/template/dto/SessionModel.java
@@ -3,10 +3,22 @@ package uz.colibri.template.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class SessionModel {
     private String sessionId;
+
+    public String generateSessionId(){
+        this.sessionId = UUID.randomUUID().toString();
+        return this.sessionId;
+    }
+    public String generateFromString(String string){
+        this.sessionId = UUID.fromString(string).toString();
+        return this.sessionId;
+    }
 }
